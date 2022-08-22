b2aabc1b0edab7745c3e316907355029e282dea7
Jeremy Tucker <jeremytucker76@gmail.com>
2022-08-22 17:49:33 -0400

    operator and radius hw

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,4 +1,4 @@
-﻿var a = 17;
+﻿int a = 17;
 var b = 4;
 
 var div = a / b;
@@ -6,6 +6,53 @@ var mod = a % b;
 
 
 Console.WriteLine($"{a}/{b} is {div} remainder {mod}.");
+
+// Exercise 2
+
+
+
+static double AreaOfCircle(double r)
+{
+  return Math.PI * r * r;
+}
+
+/*var answer = AreaOfCircle(13.0);
+Console.WriteLine(answer);*/
+Console.WriteLine("Enter the Radius");
+var userEntry = double.Parse(Console.ReadLine());
+
+Console.WriteLine(AreaOfCircle(userEntry)); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
